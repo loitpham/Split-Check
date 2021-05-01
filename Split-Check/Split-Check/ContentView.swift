5f3fabe8403b3b7aaae249107a485b206af1067c
Loi Pham <loitpham@gmail.com>
2021-05-01 12:20:07 -0700

    Added a horizontal segmented control picker for tip percentages.

--- Split-Check/Split-Check/ContentView.swift
+++ Split-Check/Split-Check/ContentView.swift
@@ -20,11 +20,21 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
+                    
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0) people")
                         }
                     }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
                 
                 Section {
