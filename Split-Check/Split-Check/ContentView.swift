b26a9619318dec2475738cd1cdc85ab3f0711a36
Loi Pham <loitpham@gmail.com>
2021-05-01 13:36:52 -0700

    Added a section for total amount with tip included.

--- Split-Check/Split-Check/ContentView.swift
+++ Split-Check/Split-Check/ContentView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+// MARK: TODO
+// - Custom percentage
+
+
+
 struct ContentView: View {
     @State private var checkAmount = ""
     @State private var numberOfPeople = 2
@@ -14,13 +19,18 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
-    var splitAmount: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+    var totalAmount: Double {
         let tipSelection = Double(tipPercentages[tipPercentage])
         let amount = Double(checkAmount) ?? 0
-        
-        return amount * (100 + tipSelection) / (100 * peopleCount)
+        return amount * (100 + tipSelection) / 100
     }
+    
+    var splitAmount: Double {
+        let peopleCount = Double(numberOfPeople) + 2
+        return totalAmount / peopleCount
+    }
+    
+
     
     var body: some View {
         NavigationView {
@@ -31,29 +41,31 @@ struct ContentView: View {
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
-                            Text("\($0) people")
+                            Text("\($0)")
                         }
                     }
-                }
+                } // SECTION
                 
-                Section(header: Text("How much tip do you want to leave?")) {
+                Section(header: Text("How much tip would you like to give?")) {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0..<tipPercentages.count) {
                             Text("\(tipPercentages[$0])%")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
-                }
+                } // SECTION
                 
-                // Formula: checkAmount x (100+percentage)/100 / numberOfPeople
-                Section {
+                Section(header: Text("Total amount with tip included")) {
+                    Text("$\(totalAmount, specifier: "%.2f")")
+                } // SECTION
+                
+                Section(header: Text("Amount per person")) {
                     Text("$\(splitAmount, specifier: "%.2f")")
-                }
-            }
+                } // SECTION
+            } // FORM
             .navigationBarTitle("Check Split")
-        }
-        
-    }
+        } // NAVIGATION-VIEW
+    } // BODY
 }
 
 struct ContentView_Previews: PreviewProvider {
